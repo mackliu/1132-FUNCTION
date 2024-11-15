6534ec56a4c637ef2eb9a36970ee3959572f5729
mackliu <macklun@gmail.com>
2024-11-15 11:42:03 +0800

    建立all()函式來取得資料表內容

--- function.php
+++ function.php
@@ -44,4 +44,12 @@ switch($shape){
 }
 }
 
+function all($table){
+    $dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+    $pdo=new PDO($dsn,'root','');
+    $sql="select * from $table";
+    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+
 ?>
